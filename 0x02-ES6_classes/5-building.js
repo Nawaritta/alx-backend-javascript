20bde8ab3d5efdfb14c31e5ae721042d43b92114
Nawaritta <Nor.benhaddou@gmail.com>
2024-01-05 01:15:20 +0000

    tasks 4 to 7

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    this.evacuationWarningMessage();
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  /* eslint-disable */
+    evacuationWarningMessage() {
+      if (this.constructor.name !== 'Building'){
+         throw new Error('Class extending Building must override evacuationWarningMessage');
+    }}
+  }
+
