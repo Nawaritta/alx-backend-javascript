bbe51efdb5dbbb39376f34b67255e1b44b42e1ab
Nawaritta <Nor.benhaddou@gmail.com>
2024-01-05 19:14:12 +0000

    fixing task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,9 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') { throw new TypeError('Name must be a string'); }
+    if (typeof length !== 'string') { throw new TypeError('Length must be a number'); }
+    if (typeof students !== 'string') { throw new TypeError('Students must be an array of strings'); }
+
     this._name = name;
     this._length = length;
     this._students = students;
