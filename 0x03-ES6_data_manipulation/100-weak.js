2011041353c83e0131942966a012cb326de41cac
Nawaritta <Nor.benhaddou@gmail.com>
2024-01-09 19:51:48 +0000

    task 9

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ const weakMap = new WeakMap();
 
 const queryAPI = (endpoint) => {
   let count = weakMap.get(endpoint) || 0;
-  weakMap.set(endpoint, ++count);
+  weakMap.set(endpoint, count++);
   if (count >= 5) throw new Error('Endpoint load is high');
   return count;
-}
+};
