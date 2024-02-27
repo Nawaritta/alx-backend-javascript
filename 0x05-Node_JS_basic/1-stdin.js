c75543d85f6fe68cd1dc2e04f3885d3f7aef9560
Nawaritta <nor.benhaddou@gmail.com>
2024-02-27 22:51:48 +0000

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
 // program executed through command line
 console.log('Welcome to Holberton School, what is your name?');
-
-process.stdin.once('data', (data) => {
-  const name = data.toString();
-  console.log('Your name is:', name);
-  console.log('This important software is now closing');
-  process.exit();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
+process.stdin.on('end', () => console.log('This important software is now closing'));
