71ad88f8053768479066fe87d82d31b4a42e0770
Nawaritta <nor.benhaddou@gmail.com>
2024-02-27 18:04:07 +0000

    task 0 eslint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
 // program executed through command line
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.once('data', function(data) {
-    const name = data.toString();
-    console.log("Your name is:", name);
-    console.log("This important software is now closing");
-    process.exit();
+process.stdin.once('data', (data) => {
+  const name = data.toString();
+  console.log('Your name is:', name);
+  console.log('This important software is now closing');
+  process.exit();
 });
