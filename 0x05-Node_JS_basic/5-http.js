4a2a363bd7dd4a928bd4fe4d237d52dd1ec5e68a
Nawaritta <nor.benhaddou@gmail.com>
2024-02-27 19:50:12 +0000

    task 7 and 5 correct output

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,67 +1,65 @@
-//small HTTP server using the http module
+// small HTTP server using the http module
 
-const fs = require('fs');
 const http = require('http');
+
 const port = 1245;
-
-const app = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    const url = req.url;
-
-    switch (url) {
-    case '/':
-        res.end('Hello Holberton School!');
-        break;
-    case '/students':
-	const path = process.argv[2];
-	countStudents(path)
-	    .then((output) => {
-		res.end(output);
-	    })
-            .catch((error) => {
-		console.log(error);
-	    });
-        break;
-    }
-});
+const fs = require('fs');
 
 function countStudents(path) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf8', (err, data) => {
-            if (err) {
-                reject(new Error('Cannot load the database'));
-                return;
-            }
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
 
-            const lines = data.trim().split('\n').filter(line => line.trim() !== '');
-            const students = lines.slice(1).map(line => line.split(','));
+      const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1).map((line) => line.split(','));
 
-            const fields = {};
-            let totalStudents = 0;
+      const fields = {};
+      let totalStudents = 0;
 
-            for (const student of students) {
-                const [firstname, lastname, age, field] = student;
-                if (!fields[field]) {
-                    fields[field] = [];
-                }
-                fields[field].push(firstname);
-                totalStudents++;
-            }
-	    let ret = ''
-	    ret += 'This is the list of our students\n';
-	    ret += `Number of students: ${totalStudents}`;
-            for (const field in fields) {
-                const count = fields[field].length;
-                const list = fields[field].join(', ');
-		ret += `Number of students in ${field}: ${count}. List: ${list}`;
-            }
+      for (const student of students) {
+        const [firstname, , , field] = student;
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+        totalStudents += 1;
+      }
+      let ret = '';
+      ret += 'This is the list of our students\n';
+      ret += `Number of students: ${totalStudents}\n`;
+      // eslint-disable-next-line guard-for-in
+      for (const field in fields) {
+        const count = fields[field].length;
+        const list = fields[field].join(', ');
+        ret += `Number of students in ${field}: ${count}. List: ${list}\n`;
+      }
 
-            resolve(ret);
-        });
+      resolve(ret.slice(0, -1));
     });
+  });
 }
 
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  const { url } = req;
 
+  if (url === '/') {
+    res.end('Hello Holberton School!');
+  }
+  if (url === '/students') {
+    const path = process.argv[2];
+    countStudents(path)
+      .then((output) => {
+        res.end(output);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+});
 
 app.listen(port);
 module.exports = app;
--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,27 +1,65 @@
 // small HTTP server using Express module
 
 const express = require('express');
+
 const port = 1245;
 const app = express();
 const path = process.argv[2];
 const fs = require('fs');
 
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1).map((line) => line.split(','));
+
+      const fields = {};
+      let totalStudents = 0;
+
+      for (const student of students) {
+        const [firstname, , , field] = student;
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+        totalStudents += 1;
+      }
+      let ret = '';
+      ret += 'This is the list of our students\n';
+      ret += `Number of students: ${totalStudents}\n`;
+      // eslint-disable-next-line guard-for-in
+      for (const field in fields) {
+        const count = fields[field].length;
+        const list = fields[field].join(', ');
+        ret += `Number of students in ${field}: ${count}. List: ${list}\n`;
+      }
+
+      resolve(ret.slice(0, -1));
+    });
+  });
+}
+
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-    countStudents(path)
-        .then((output) => {
-            res.send(output);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+  countStudents(path)
+    .then((output) => {
+      res.send(output);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
 
 app.use((req, res) => {
-    res.status(404).send(`<!DOCTYPE html>
+  res.status(404).send(`<!DOCTYPE html>
     <html lang="en">
     <head>
     <meta charset="utf-8">
@@ -32,42 +70,6 @@ app.use((req, res) => {
     </body>
     </html>`);
 });
-
-function countStudents(path) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf8', (err, data) => {
-            if (err) {
-                reject(new Error('Cannot load the database'));
-                return;
-            }
-
-            const lines = data.trim().split('\n').filter(line => line.trim() !== '');
-            const students = lines.slice(1).map(line => line.split(','));
-
-            const fields = {};
-            let totalStudents = 0;
-
-            for (const student of students) {
-                const [firstname, lastname, age, field] = student;
-                if (!fields[field]) {
-                    fields[field] = [];
-                }
-                fields[field].push(firstname);
-                totalStudents++;
-            }
-            let ret = ''
-            ret += 'This is the list of our students\n';
-            ret += `Number of students: ${totalStudents}`;
-            for (const field in fields) {
-                const count = fields[field].length;
-                const list = fields[field].join(', ');
-                ret += `Number of students in ${field}: ${count}. List: ${list}`;
-            }
-
-            resolve(ret);
-        });
-    });
-}
 
 app.listen(port);
 module.exports = app;
