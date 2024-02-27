5f11d6e911611dda6e6ccb16ca18d7ab09e4118d
Nawaritta <nor.benhaddou@gmail.com>
2024-02-27 21:25:06 +0000

    use then catch

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,44 +3,45 @@
 const http = require('http');
 
 const port = 1245;
-const fs = require('fs');
+const fs = require('fs').promises;
 
 async function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-      }
+    fs.readFile(path, 'utf-8')
+      .then((data) => {
+        const lines = data
+          .trim()
+          .split('\n')
+          .filter((line) => line.trim() !== '');
+        const students = lines.slice(1).map((line) => line.split(','));
 
-      const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
-      const students = lines.slice(1).map((line) => line.split(','));
+        const fields = {};
+        let totalStudents = 0;
 
-      const fields = {};
-      let totalStudents = 0;
-
-      for (const student of students) {
-        const [firstname, , , field] = student;
-        if (!fields[field]) {
-          fields[field] = [];
+        for (const student of students) {
+          const [firstname, , , field] = student;
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstname);
+          totalStudents += 1;
         }
-        fields[field].push(firstname);
-        totalStudents += 1;
-      }
-      let ret = '';
-      ret += 'This is the list of our students\n';
-      ret += `Number of students: ${totalStudents}\n`;
-      // eslint-disable-next-line guard-for-in
-      for (const field in fields) {
-        const count = fields[field].length;
-        const list = fields[field].join(', ');
-        ret += `Number of students in ${field}: ${count}. List: ${list}\n`;
-      }
-
-      resolve(ret.slice(0, -1));
-    });
+        let ret = '';
+        ret += 'This is the list of our students\n';
+        ret += `Number of students: ${totalStudents}\n`;
+        // eslint-disable-next-line guard-for-in
+        for (const field in fields) {
+          const count = fields[field].length;
+          const list = fields[field].join(', ');
+          ret += `Number of students in ${field}: ${count}. List: ${list}\n`;
+        }
+        resolve(ret.slice(0, -1));
+      })
+      .catch(() => {
+        reject(new Error('Cannot load the database'));
+      });
   });
 }
-
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   const { url } = req;
@@ -55,7 +56,7 @@ const app = http.createServer((req, res) => {
         res.end(output);
       })
       .catch((error) => {
-        res.end(`${error}`);
+        res.end(`This is the list of our students\n${error}`);
       });
   }
 });
