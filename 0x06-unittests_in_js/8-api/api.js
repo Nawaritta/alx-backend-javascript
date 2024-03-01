380ddb041e68542d8983ab5a0b72a32244f0afa6
Nawaritta <nor.benhaddou@gmail.com>
2024-03-01 22:46:51 +0000

    correct status

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 7865;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
+  res.status(200).send('Welcome to the payment system');
 });
 
 const server = app.listen(port, () => {
