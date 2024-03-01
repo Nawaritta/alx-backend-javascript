ee16fa019e6224b58273b72d2149d53eb4b7b42c
Nawaritta <nor.benhaddou@gmail.com>
2024-03-01 01:02:20 +0000

    modify the tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,9 +3,19 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('return sum of rounded numbers negtive floats', () => {
-    assert.equal(calculateNumber(-1.7, -2), -4);
+    assert.equal(calculateNumber(-2.8, -2), -5);
   });
   it('return sum of rounded numbers', () => {
-    assert.equal(calculateNumber(-1, -2.7), -4);
+    assert.equal(calculateNumber(-1, -2.8), -4);
+  });
+  it('rounds the first number', () => {
+    assert.equal(calculateNumber(1.0, 0), 1);
+    assert.equal(calculateNumber(1.2, 0), 1);
+    assert.equal(calculateNumber(1.8, 0), 2);
+  });
+  it('rounds the second number', () => {
+    assert.equal(calculateNumber(0, 1.0), 1);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(0, 1.8), 2);
   });
 });
