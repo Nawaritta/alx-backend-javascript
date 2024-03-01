1276ab12ef356d8a4b8abeaaddc21211868d61b1
Nawaritta <nor.benhaddou@gmail.com>
2024-03-01 00:34:28 +0000

    only two neg

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -11,49 +11,50 @@ describe('calculateNumber', () => {
   });
   it('return sum of rounded numbers', () => {
     assert.equal(calculateNumber(-1, -2.7), -4);
+
   });
 
-  it('return sum of rounded numbers', () => {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-  });
+  // it('return sum of rounded numbers', () => {
+  //   assert.equal(calculateNumber(1.2, 3.7), 5);
+  // });
 
-  it('add negative numbers', () => {
-    assert.equal(calculateNumber(-1, -4.8), -6);
-  });
+  // it('add negative numbers', () => {
+  //   assert.equal(calculateNumber(-1, -4.8), -6);
+  // });
 
-  it('add a positive and a negative numbers', () => {
-    assert.equal(calculateNumber(-1.5, 2), 1);
-  });
-  it('add a zero to a negative number', () => {
-    assert.equal(calculateNumber(-1.5, 0), -1);
-  });
+  // it('add a positive and a negative numbers', () => {
+  //   assert.equal(calculateNumber(-1.5, 2), 1);
+  // });
+  // it('add a zero to a negative number', () => {
+  //   assert.equal(calculateNumber(-1.5, 0), -1);
+  // });
 
-  it('add a zero to a positive number', () => {
-    assert.equal(calculateNumber(1.5, 0), 2);
-  });
+  // it('add a zero to a positive number', () => {
+  //   assert.equal(calculateNumber(1.5, 0), 2);
+  // });
 
-  it('add two int', () => {
-    assert.equal(calculateNumber(1, 3), 4);
-  });
+  // it('add two int', () => {
+  //   assert.equal(calculateNumber(1, 3), 4);
+  // });
 
-  it('add int and float', () => {
-    assert.equal(calculateNumber(1, 3.7), 5);
-  });
+  // it('add int and float', () => {
+  //   assert.equal(calculateNumber(1, 3.7), 5);
+  // });
 
-  it('add floats', () => {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-  });
+  // it('add floats', () => {
+  //   assert.equal(calculateNumber(1.5, 3.7), 6);
+  // });
 
-  //
-  it('add a positive and a negative numbers', () => {
-    assert.equal(calculateNumber(2, -1.5), 1);
-  });
-  it('add a zero to a negative number', () => {
-    assert.equal(calculateNumber(0, -1.5), -1);
-  });
+  // //
+  // it('add a positive and a negative numbers', () => {
+  //   assert.equal(calculateNumber(2, -1.5), 1);
+  // });
+  // it('add a zero to a negative number', () => {
+  //   assert.equal(calculateNumber(0, -1.5), -1);
+  // });
 
-  it('add a zero to a positive number', () => {
-    assert.equal(calculateNumber(0, 1.5), 2);
-  });
+  // it('add a zero to a positive number', () => {
+  //   assert.equal(calculateNumber(0, 1.5), 2);
+  // });
 
 });
