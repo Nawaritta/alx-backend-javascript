4c99feec3fa2b0efbf583070ed19bbbdf77b1022
Nawaritta <nor.benhaddou@gmail.com>
2024-03-01 00:59:49 +0000

    test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,13 +1,11 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
-const{it, describe} = require('mocha');
 
 describe('calculateNumber', () => {
   it('return sum of rounded numbers negtive floats', () => {
-    assert.equal(calculateNumber(-2.8, -2), -5);
+    assert.equal(calculateNumber(-1.7, -2), -4);
   });
   it('return sum of rounded numbers', () => {
-    assert.equal(calculateNumber(-1, -2.8), -4);
-
+    assert.equal(calculateNumber(-1, -2.7), -4);
   });
 });
