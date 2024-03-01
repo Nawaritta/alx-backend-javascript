997dbdaaa509725ab4f618c044e208b90bc5d366
Nawaritta <nor.benhaddou@gmail.com>
2024-03-01 01:20:13 +0000

    remove the rejection

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,9 +1,8 @@
 function getPaymentTokenFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ data: 'Successful response from the API' });
-    }
-  });
-}
-
-module.exports = getPaymentTokenFromAPI;
+    return new Promise((resolve) => {
+      if (success) {
+        resolve({ data: 'Successful response from the API' });
+      }
+    });
+  }
+  module.exports = getPaymentTokenFromAPI;
